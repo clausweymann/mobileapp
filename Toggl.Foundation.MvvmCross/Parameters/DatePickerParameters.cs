46edcc0de5051252241b01582576715c02635aaa
kornelijepetak <kornelije@gmail.com>
2018-02-01 17:46:25 +0100

    :construction: Refactor SelectDateTimeViewModel

--- Toggl.Foundation.MvvmCross/Parameters/DatePickerParameters.cs
+++ Toggl.Foundation.MvvmCross/Parameters/DatePickerParameters.cs
@@ -6,18 +6,28 @@ namespace Toggl.Foundation.MvvmCross.Parameters
     [Preserve(AllMembers = true)]
     public sealed class DatePickerParameters
     {
+        public PickerMode Mode { get; set; }
+
         public DateTimeOffset CurrentDate { get; set; }
 
         public DateTimeOffset MinDate { get; set; }
 
         public DateTimeOffset MaxDate { get; set; }
 
-        public static DatePickerParameters WithDates(DateTimeOffset current, DateTimeOffset min, DateTimeOffset max) => 
+        public static DatePickerParameters WithDates(PickerMode mode, DateTimeOffset current, DateTimeOffset min, DateTimeOffset max) =>
             new DatePickerParameters
             {
+                Mode = mode,
                 CurrentDate = current,
                 MinDate = min,
                 MaxDate = max
             };
+
+        public enum PickerMode
+        {
+            Date,
+            Time,
+            DateTime
+        }
     }
 }
